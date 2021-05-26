7e9d9e8ccb51396ffa117617e8c3a62eaf24868f
jitendradev <jitendra.panwar2002@gmail.com>
2021-05-26 14:52:33 +1000

    Added the correct AZ

--- myec2.tf
+++ myec2.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "ap-southeast-2a"
 
   tags = {
     Name = "terraform-cloud"
