1689d256ad35c98745c8980921f57069a3332185
jitendradev <jitendra.panwar2002@gmail.com>
2021-05-26 14:49:48 +1000

    Added the VPCID and other information

--- myec2.tf
+++ myec2.tf
@@ -1,25 +1,36 @@
  resource "aws_instance" "myec2v1" {
    ami = "ami-09f0bb8545fdc6d7c"
    instance_type = var.instance_type
+   network_interface {
+    network_interface_id = aws_network_interface.tf-cloud.id
+    device_index         = 0
+  }
+
 } 
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    description = "SSH into VPC"
-    from_port   = 23
-    to_port     = 23
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "terraform-cloud"
   }
-  egress {
-    description = "Outbound Allowed"
-    from_port   = 1024
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "terraform-cloud"
+  }
+}
+
+resource "aws_network_interface" "tf-cloud" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
   }
 }
